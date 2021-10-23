686480eb5b15eedfc5747cc71661baaf2979b8f0
kay <kayshiro@live.no>
2021-10-23 21:39:46 +0200

    error handling done

--- app/src/main/java/com/kay/bysykkeldata/ui/BySykkelFragment.kt
+++ app/src/main/java/com/kay/bysykkeldata/ui/BySykkelFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kay.bysykkeldata.databinding.BySykkelFragmentBinding
@@ -41,15 +44,28 @@ class BySykkelFragment : Fragment() {
 
         // observe LiveData
         viewModel.liveData.observe(viewLifecycleOwner, {setupList(it)})
+        viewModel.errorLiveData.observe(viewLifecycleOwner){
+            if (it == null){
+                // if the error is null, show the list and hide the error textview
+                binding.noConnectionTv.isVisible = false
+                binding.recyclerView.isVisible = true
+            } else {
+                // if the error is not null, show the error text view and hide the list
+                binding.recyclerView.isVisible = false
+                binding.noConnectionTv.isVisible = true
+            }
+        }
     }
 
     private fun setupList(data: List<BySykkelUiModel>) {
         adapter.setData(data)
-        binding.recyclerView
+        binding.swipeRefreshLayout.isRefreshing = false
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 }
+// TODO: set the list to visible if the error is null, and set the error text view to visible if the error live data is not null
--- app/src/main/java/com/kay/bysykkeldata/ui/BySykkelViewModel.kt
+++ app/src/main/java/com/kay/bysykkeldata/ui/BySykkelViewModel.kt
@@ -1,5 +1,7 @@
 package com.kay.bysykkeldata.ui
 
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,15 +12,17 @@ import java.lang.Exception
 class BySykkelViewModel(private val repo: BySykkelRepository): ViewModel() {
 
     var liveData = MutableLiveData<List<BySykkelUiModel>>()
+    var errorLiveData = MutableLiveData<String?>()
 
 
     fun getInfo() {
         viewModelScope.launch {
             try {
-                 var list = repo.getData()
+                 val list = repo.getData()
                 liveData.postValue(list)
+                errorLiveData.postValue(null)
             } catch (e:Exception){
-
+                errorLiveData.postValue("error")
             }
         }
     }
